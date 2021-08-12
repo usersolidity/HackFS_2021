f7cf4c2ca76a19f3dd452cc691d61959b75b9147
eckchristian0 <66914749+eckchristian0@users.noreply.github.com>
2021-08-12 11:21:57 -0400

    Updated Functions and Fixed Error

--- HTML_JS/nftStorage.js
+++ HTML_JS/nftStorage.js
@@ -5,16 +5,23 @@ const nftClient = new NFTStorage.NFTStorage({ token: NFTSTORAGE_API_TOKEN });
 const Web3 = require('web3');
 
 var web3 = new Web3(Web3.givenProvider);
-var contractInstance;
+var AssetInstance;
+var ContractInstance;
 
 $(document).ready(function () {
   window.ethereum.enable().then(function (accounts) {    //launches metamask to ask for connecting account
     //Contract(abi (create .js file to import),address(get from console migrate "string format"), from{account[0]})
     // The address supplied is the contract this will interact with
-    contractInstance = new web3.eth.Contract(abi, "0xB9bcF5bEa4266BB28b654661D1d2949456e5BF0E", { from: accounts[0] });
-
+    AssetInstance = new web3.eth.Contract(abi, "0x55ad78988D3A09e0960c40A467474FF9D4C91B2a", { from: accounts[0] });
+    MarketInstance = new web3.eth.Contract(abi2, "0xd44Ae8f59F640f25690E7Ff63e3F2d9429aD4cB5", { from: accounts[0] });
+    const MarketConractAddress = 0xd44Ae8f59F640f25690E7Ff63e3F2d9429aD4cB5;
 
     $("#mint").click(mint);
+    //ApproveMarketplace
+    //SetOffer (GWEI Per Period, TokenId)
+    //RemoveOffer (TokenId)
+    //LendAsset (tokenId, GWEI to Cover Cost)
+    //ReturnAsset (TokenId)
 
     async function mint() {
       var metadataURI = await client.store({
@@ -30,10 +37,38 @@ $(document).ready(function () {
       })
       console.log(metadataURI.url)
 
-      contractInstance.methods.createAsset(metadataURI);
+      AssetInstance.methods.createAsset(metadataURI);
     };
 
-    contractInstance.events.NewAssetCreation({ fromBlock: 'latest' }, function (res) {
+    //Create function call ApprovalForAll Function 
+    //This will allow the user to approve the P2P contract for Lending (AssetInstance)
+    function ApproveMarketplace() {
+      AssetInstance.methods.setApprovalForAll( MarketConractAddress, true)
+    }
+
+  
+    //Create function call setOffer (MarketInstance)
+    function SetOffer() {
+      MarketInstance.methods.setOffer($("Gwei"), $("tokenId") );
+    }
+
+
+    //Create function call lendAsset (Payable) (MarketInstance)
+    //Get the GWEI per period and make that msg.value
+
+
+    //Create function call removeOffer (MarketInstance)
+    function RemoveOffer(){
+      MarketInstance.methods.removeOffer($("tokenId"));
+    }
+
+    //Create function call returnAsset (MarketInstance)
+    function ReturnAsset(){
+      MarketInstance.methods.returnAsset($("tokenId"));
+    }
+
+    //Event that logs the creation of an Asset by Asset Contract
+    AssetInstance.events.NewAssetCreation({ fromBlock: 'latest' }, function (res) {
       console.log(res);
     });
   })
