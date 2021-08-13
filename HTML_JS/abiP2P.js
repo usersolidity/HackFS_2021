b26f4c804f54bceb82a5c0f3b613a8fa58da0846
eckchristian0 <66914749+eckchristian0@users.noreply.github.com>
2021-08-13 17:58:57 -0400

    Updated the htmls to have multiple pages

---
+++ HTML_JS/abiP2P.js
@@ -0,0 +1,219 @@
+var abi =
+[
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "TxType",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "MarketTransaction",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_AssetContractAddress",
+        "type": "address"
+      }
+    ],
+    "name": "setAssetContract",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getOffer",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "lender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "GWEIPerPeriod",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "activeOffering",
+        "type": "bool"
+      },
+      {
+        "internalType": "bool",
+        "name": "activelyBorrowed",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "getAllTokenOnOffer",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "listOfOffers",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getTokenGWEI",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "price",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_GWEIPerPeriod",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "setOffer",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "removeOffer",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "lendAsset",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "returnAsset",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amountToTransfer",
+        "type": "uint256"
+      }
+    ],
+    "name": "Withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "contractBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+]
--- HTML_JS/nftStorage.js
+++ HTML_JS/nftStorage.js
@@ -1,27 +1,29 @@
+
 // import { NFTStorage, File } from 'nft.storage'
 const NFTStorage = require('nft.storage')
 const NFTSTORAGE_API_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEJFNjAzNjYxMzk2OTg5Rjk5YUU4MURGNzRGY2NFRDQ4YUFGMkYyOGEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyODE5MjE3ODUzMCwibmFtZSI6IkhhY2tGU18yMDIxIn0.8egYuYRc_nmWS-HyEbSYliB2ShACUq2TWEswzcnoOVI'
 const nftClient = new NFTStorage.NFTStorage({ token: NFTSTORAGE_API_TOKEN });
-const Web3 = require('web3');
+const Web3 = require('@metamask/legacy-web3');
 
 var web3 = new Web3(Web3.givenProvider);
 var AssetInstance;
 var ContractInstance;
 
-$(document).ready(function () {
+$(function () {
   window.ethereum.enable().then(function (accounts) {    //launches metamask to ask for connecting account
     //Contract(abi (create .js file to import),address(get from console migrate "string format"), from{account[0]})
     // The address supplied is the contract this will interact with
-    AssetInstance = new web3.eth.Contract(abi, "0x55ad78988D3A09e0960c40A467474FF9D4C91B2a", { from: accounts[0] });
-    MarketInstance = new web3.eth.Contract(abi2, "0xd44Ae8f59F640f25690E7Ff63e3F2d9429aD4cB5", { from: accounts[0] });
+    AssetInstance = new web3.eth.Contract(abiAsset, "0x9E5D45f830625F269F01b74B326d9f6c641b1946", { from: accounts[0] });
+    MarketInstance = new web3.eth.Contract(abiP2P, "0xeC563222B0b2F61177b0f65493BF4CE725F6E23b", { from: accounts[0] });
     const MarketConractAddress = 0xd44Ae8f59F640f25690E7Ff63e3F2d9429aD4cB5;
 
-    $("#mint").click(mint);
-    //ApproveMarketplace
-    //SetOffer (GWEI Per Period, TokenId)
-    //RemoveOffer (TokenId)
-    //LendAsset (tokenId, GWEI to Cover Cost)
-    //ReturnAsset (TokenId)
+    $("#mint").onclick(mint);
+    $("#approve").onclick(ApproveMarketplace);
+    $("#SetOffer").onclick(SetOffer);
+    $("#RemoveOffer").onclick(RemoveOffer);
+    //GetOffers
+    $("#LendAsset").onclick(LendAsset);
+    $("#ReturnAsset").onclick(ReturnAsset);
 
     async function mint() {
       var metadataURI = await client.store({
@@ -49,26 +51,34 @@ $(document).ready(function () {
   
     //Create function call setOffer (MarketInstance)
     function SetOffer() {
-      MarketInstance.methods.setOffer($("Gwei"), $("tokenId") );
+      MarketInstance.methods.setOffer($("#Gwei"), $("#TokenIdset") );
     }
 
 
     //Create function call lendAsset (Payable) (MarketInstance)
-    //Get the GWEI per period and make that msg.value
-
+    //Get the GWEI and make that msg.value
+    function LendAsset(){
+      var gweiAmount = MarketInstance.methods.getTokenGWEI($("TokenIdborrow"));
+      MarketInstance.methods.LendAsset($("TokenIdborrow"), {value:web3.utils.toWei(gweiAmount,"gwei")});
+    }
 
     //Create function call removeOffer (MarketInstance)
     function RemoveOffer(){
-      MarketInstance.methods.removeOffer($("tokenId"));
+      MarketInstance.methods.removeOffer($("tokenIdremove"));
     }
 
     //Create function call returnAsset (MarketInstance)
     function ReturnAsset(){
-      MarketInstance.methods.returnAsset($("tokenId"));
+      MarketInstance.methods.returnAsset($("tokenIdreturn"));
     }
 
     //Event that logs the creation of an Asset by Asset Contract
     AssetInstance.events.NewAssetCreation({ fromBlock: 'latest' }, function (res) {
+      console.log(res);
+    });
+    
+    //Event that logs the creation of P2PMarket Contract
+    MarketInstance.events.MarketTransaction({ fromBlock: 'latest' }, function (res) {
       console.log(res);
     });
   })
